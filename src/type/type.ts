9f2b121aa5d7fcf32e962e4d09c0ae6e0bde6881
eunsu <songess@naver.com>
2024-10-05 22:29:07 +0900

    [feat] : type.ts 파일 추가 - 블로그 포스트와 스니펫 타입 정의

---
+++ src/type/type.ts
@@ -0,0 +1,16 @@
+// 블로그 포스트 타입 정의
+export type Post = {
+  id: string;
+  title: string;
+  excerpt: string;
+  date: string;
+  category: 'All' | '운영체제' | 'FE' | 'CS' | '일상';
+};
+
+// 스니펫 타입 정의
+export type Snippet = {
+  id: number;
+  title: string;
+  tags: string[];
+  date: string;
+};
