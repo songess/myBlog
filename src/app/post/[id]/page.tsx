feca3851df694a6fd0a5e8610c83b1c135cf3b5a
eunsu <songess@naver.com>
2024-10-28 16:58:19 +0900

    [fix] : post/[id]/page.tsx 수정

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,8 +1,14 @@
-import { parsePostDetail } from '@/lib/parsing';
+import { getSortedPostList, parsePost } from '@/lib/parsingPost';
 import React from 'react';
 
-export default async function Page() {
-  const { data, content } = await parsePostDetail('contents.mdx');
-  // console.log(data, content);
-  return <div>{content}</div>;
+const BASE_PATH = 'public/posts/post';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  console.log(params);
+  const p = await parsePost(`${BASE_PATH}/contents.mdx`);
+  const c = await getSortedPostList('post');
+  return;
+  <>
+    <div>{p.content}</div>;
+  </>;
 }
