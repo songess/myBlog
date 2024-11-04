ae72672b2fa6004fc11dcf424f2a3ed21a966ddf
eunsu <songess@naver.com>
2024-11-04 18:02:11 +0900

    [feat] : 동적라우팅 페이지 생성

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,14 +1,14 @@
 import PostBody from '@/components/ui/PostBody';
-import { BASE_POSTS_PATH } from '@/constant/path';
+import { BASE_SNIPPETS_PATH } from '@/constant/path';
 import { getSortedPostList, parsePost } from '@/lib/parsingPost';
 import React from 'react';
 
 export default async function Page({ params }: { params: { id: string } }) {
-  console.log('params', params);
-  const BASE_PATH = BASE_POSTS_PATH;
-  const p = await parsePost(`${BASE_PATH}/contents.mdx`);
+  console.log(params);
+  const BASE_PATH = BASE_SNIPPETS_PATH;
+  const p = await parsePost(`${BASE_PATH}/contentss.mdx`);
   const c = await getSortedPostList('posts');
-  console.log('p,c', p, c);
+  console.log(p, c);
   return (
     <div className="px-5">
       <PostBody contentData={p.content} />
