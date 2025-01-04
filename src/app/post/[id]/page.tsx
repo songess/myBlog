e56e0ace8cb556e8205c0cb5a5c6815359d9188d
eunsu <songess@naver.com>
2025-01-04 20:08:19 +0900

    [feat] : post 페이지에 적용

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,6 +2,7 @@ import CategoryTag from '@/components/all/CategoryTag';
 import Giscus from '@/components/all/Giscus';
 import PostIndex from '@/components/all/PostIndex';
 import PostBody from '@/components/ui/PostBody';
+import ProgressBar from '@/components/post/ProgressBar';
 import { BASE_POSTS_PATH } from '@/constant/path';
 import { parsePost } from '@/lib/parseContent';
 import dayjs from 'dayjs';
@@ -13,25 +14,28 @@ export default async function Page({ params }: { params: { id: string } }) {
   const p = await parsePost(`${BASE_PATH}/${params.id}.mdx`);
 
   return (
-    <div className="px-5 container mx-auto max-w-[768px]">
-      <Image
-        src={p.image}
-        alt={`${p.title} 썸네일 이미지`}
-        width={100}
-        height={100}
-        className="object-center w-fit h-36 mb-2 mx-auto"
-      />
-      <div className="flex flex-col gap-2 mb-2">
-        <CategoryTag category={p.category} />
-        <h1 className="text-[40px]">{p.title}</h1>
-        <p className="text-gray-600 text-sm">
-          {dayjs(p.date).format('YYYY. MM. DD')}
-        </p>
+    <>
+      <ProgressBar />
+      <div className="px-5 container mx-auto max-w-[768px] relative">
+        <Image
+          src={p.image}
+          alt={`${p.title} 썸네일 이미지`}
+          width={100}
+          height={100}
+          className="object-center w-fit h-36 mb-2 mx-auto"
+        />
+        <div className="flex flex-col gap-2 mb-2">
+          <CategoryTag category={p.category} />
+          <h1 className="text-[40px]">{p.title}</h1>
+          <p className="text-gray-600 text-sm">
+            {dayjs(p.date).format('YYYY. MM. DD')}
+          </p>
+        </div>
+        <hr className="py-2" />
+        <PostIndex />
+        <PostBody contentData={p.content} />
+        <Giscus />
       </div>
-      <hr className="py-2" />
-      <PostIndex />
-      <PostBody contentData={p.content} />
-      <Giscus />
-    </div>
+    </>
   );
 }
